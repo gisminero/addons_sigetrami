9fcbaa79890031dabc02413a557e2560cb0cb88c
dspedrozo <danielapedrozo@gmail.com>
2021-09-02 14:28:42 -0700

    modulo inversion de capital 0.0

--- exp_inv_capital/__manifest__.py
+++ exp_inv_capital/__manifest__.py
@@ -14,6 +14,9 @@
 
     'depends': ['base', 'expediente', 'sh_message'],
     'data': [
+        'security/inv_capital_security.xml',
+        'security/ir.model.access.csv',
+        'views/view_inversion_de_capital.xml'
     ],
 
     'application': True, 
