f49ff489983e0c9d8e6aa0b6e26e6650e622c949
Dario Rodriguez <dariojrodriguez2000@gmail.com>
2021-09-29 16:37:16 -0700

    trabajo miercoles 29

--- exp_pertenencias/__manifest__.py
+++ exp_pertenencias/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'expediente'], #agrego los models con los que se relaciona en este caso expediente
 
     # always loaded
     'data': [
