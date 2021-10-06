1b3211c50f2cd322bd4489c45f8b97c8a1822bd9
Dario Rodriguez <dariojrodriguez2000@gmail.com>
2021-10-06 14:23:33 -0700

    avances 6-10

--- exp_pertenencias/models/models.py
+++ exp_pertenencias/models/models.py
@@ -2,13 +2,32 @@
 from odoo import models, fields, api
 from odoo import exceptions
 
-  
+class exp_pertenencias(models.Model):
+    _name = 'exp_pertenencias'
+    _description = "Cantidad de pertenencias por expediente"
+    
+    pertenencias = fields.Integer('Cantidad de pertenencias', readonly = 0,required = False, default = 0)
+    """writegroup = fields.Boolean(string='user', compute='_compute_user_check', default=False, store=True)"""
+    exp_id = fields.Many2one('expediente.expediente', 'Pertenencias', required=1, ondelete='cascade')
+    """user_id = fields.Many2one('res.users','Current User', default=lambda self: self.env.user)"""
+
+
 class expediente(models.Model):
     _name = 'expediente.expediente'
     _inherit = 'expediente.expediente'
     _description = "Asociación con pertenencias"
+   
     
-    pertenencias= fields.Integer('Cantidad de pertenencias', readonly = 0,required = False, default = 0)
-
-
-
+    pertenencias_id = fields.One2many('exp_pertenencias', 'exp_id', string='Obligaciones', required=False)
+    
+"""     def _compute_user_check(self):
+        print("------------------------------")
+        print("---------checkGroup-----------")        
+        #user = self.env['res.users'].browse(self._uid)
+        if self.env['res.users'].browse(self.env.uid).has_group('base.access_group_exp_pertenencias_escritura'):
+            self.writegroup = True
+        else:
+            self.writegroup = False
+        print("self.writegroup: ")
+        print(self.writegroup)
+        """
