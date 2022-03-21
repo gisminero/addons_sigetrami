4cd1af71eaea774be502f71a457fd5cf6b81ac58
Dario Rodriguez <dariojrodriguez2000@gmail.com>
2022-03-21 15:08:38 -0700

    Limpiando.

--- exp_ff_admin/models/models.py
+++ exp_ff_admin/models/models.py
@@ -29,69 +29,6 @@ class exp_ff_admin(models.Model):
         return exp_obj.write(vals)
     """
 
-class pase(models.Model):
-    _name = 'pase.pase'
-    _inherit = 'pase.pase'
-    _description = "Listado de pases"
-
-    """
-    def enviados_sin_recepcion_view(self):
-        print ("BUSCANDO MI OFICINA ################################")
-        user_id = self.env.user.id
-        depart_user_id = self.userdepart(user_id)
-        if depart_user_id > 0:
-            action = {
-                'name': "Expedientes en Mi Oficina",
-                'view_mode': 'tree, form',
-                'res_model': 'pase.pase',
-                'type': 'ir.actions.act_window',
-                'domain': [('depart_origen_id', '=', depart_user_id), ('fecha_hora_recep', '=', False)],
-                'views': [[self.env.ref('exp_envios_admin.envios_no_rec_list').id, "tree"], [self.env.ref('exp_envios_admin.envios_no_rec_form').id, "form"]],
-                }
-        return action
-    # self.env['expediente.expediente'].depart_user()
-
-    def enviados_reclamados_view(self):
-        # user_id = self.env.user.id
-        # depart_user_id = self.userdepart(user_id)
-        view = self.env.ref('sh_message.sh_message_wizard_false')
-        view_id = view and view.id or False
-        context = dict(self._context or {})
-        context[
-            'message'] = 'Esta funcionalidad se encuentra en desarrollo.'
-        return {
-            'name': 'Correccion de envíos erroneos.',
-            'type': 'ir.actions.act_window',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'sh.message.wizard',
-            'views': [(view.id, 'form')],
-            'view_id': view.id,
-            'target': 'new',
-            'context': context,
-        }
-
-    def correccion_flujo_view(self):
-        print ("BUSCANDO MI OFICINA ################################")
-        user_id = self.env.user.id
-        depart_user_id = self.userdepart(user_id)
-        if depart_user_id > 0:
-            action = {
-                'name': "Administrar ubicación de documentos.",
-                'view_mode': 'tree',
-                #, form
-                'res_model': 'expediente.expediente',
-                'type': 'ir.actions.act_window',
-                # 'target': 'new',
-                'domain': [ ('recibido', '=', True)],#('ultimo_pase_id', '!=', False),
-                'views': [[self.env.ref('exp_envios_admin.expediente_corregir').id, "tree"]],
-            # , [self.env.ref('exp_cambio_tramite.expediente_corregir_form').id, "form"]
-                }
-        return action
-    """
-
-
-
 class expediente(models.Model):
     _name = 'expediente.expediente'
     _inherit = 'expediente.expediente'
